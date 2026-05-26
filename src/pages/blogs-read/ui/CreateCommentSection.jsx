03fd8da003f94ab12207a83a62e8e5c7f1e9853e
Josef Tinagan <tinaganjosefperry@gmail.com>
2026-05-26 08:42:30 +0800

    feat: implement a previous url feature to navigate back to after logging in, using search parameters, login form checks for it and will navigate to it if login is successful

--- src/pages/blogs-read/ui/CreateCommentSection.jsx
+++ src/pages/blogs-read/ui/CreateCommentSection.jsx
@@ -2,13 +2,19 @@ import { Link } from "react-router";
 
 import CommentForm from "./CommentForm";
 import { useAuth } from "../../../entities/user";
+import { useLocation } from "react-router";
 
 const CreateCommentSection = () => {
   const { isAuth } = useAuth();
+  const url = useLocation();
+  console.log("Showing url...");
+  console.log(url.pathname);
+
 
   return(
     <div>
-      {isAuth ? <CommentForm /> : <span>You need to <Link to='/sign-in'>login</Link> to post a comment.</span> }
+      {isAuth ? <CommentForm /> : <span>You need to <Link to={`/sign-in?prevUrl=${url.pathname}`}>login</Link> to post a comment.</span> }
+      <p></p>
     </div>
   )
 }
--- src/pages/sign-in/ui/LoginFormReact19.jsx
+++ src/pages/sign-in/ui/LoginFormReact19.jsx
@@ -1,107 +1,113 @@
-// Using `useActionState`
-// Login Form that uses new way to create a form from React 19's `useActionState` and passing a function to the `action` attribute of a form
-// Uses global state `isAuth` and `user`
+  // Using `useActionState`
+  // Login Form that uses new way to create a form from React 19's `useActionState` and passing a function to the `action` attribute of a form
+  // Uses global state `isAuth` and `user`
 
-import { useActionState } from "react";
-import { useNavigate } from "react-router";
+  import { useActionState } from "react";
+  import { useNavigate, useSearchParams } from "react-router";
 
-import loginUser from "../api/loginUser";
-import postLogin from "../api/postLogin";
-import { setToken } from "../../../shared/lib/tokenHelper";
-import { useAuth } from "../../../entities/user";
+  import loginUser from "../api/loginUser";
+  import postLogin from "../api/postLogin";
+  import { setToken } from "../../../shared/lib/tokenHelper";
+  import { useAuth } from "../../../entities/user";
 
-async function loginUserAction(isAuth, setIsAuth, navigate, prevState, formData){ 
-  setToken();
+  async function loginUserAction(isAuth, setIsAuth, navigate, prevUrl, prevState, formData){ 
+    setToken();
+    console.log("Prev url value:");
+    console.log(prevUrl);
 
-  const email = formData.get("user_name");
-  const data = {
-    user_name: formData.get("user_name"),
-    password: formData.get("password"),
-  }
-
-  try {
-    if(isAuth){
-      return { error: "Already logged in" };
+    const email = formData.get("user_name");
+    const data = {
+      user_name: formData.get("user_name"),
+      password: formData.get("password"),
     }
 
-    if(!email.includes('@')){
-      return { error: "Invalid email address"};
+    try {
+      if(isAuth){
+        return { error: "Already logged in" };
+      }
+
+      if(!email.includes('@')){
+        return { error: "Invalid email address"};
+      }
+      const result = await loginUser(data);
+      await postLogin(result, isAuth, setIsAuth);
+
+      setTimeout(() => {
+        navigate(prevUrl);
+      }, 2000);
+
+      // Instead of returning the below, use `useNavigate` to navigate to `/` or `/dashboard` or the previous url before the login 
+      return { error: null, success: true, message: `Login successful`};
+    } catch (error) {
+      return { error: `"Wrong Credentials and ${error}`, success: false}
     }
-    const result = await loginUser(data);
-    await postLogin(result, isAuth, setIsAuth);
 
-    setTimeout(() => {
-      navigate('/');
-    }, 2000);
 
-    // Instead of returning the below, use `useNavigate` to navigate to `/` or `/dashboard` or the previous url before the login 
-    return { error: null, success: true, message: `Login successful`};
-  } catch (error) {
-    return { error: `"Wrong Credentials and ${error}`, success: false}
   }
 
+  const LoginForm = () => {
+    const navigate = useNavigate();
+    const [searchParams]= useSearchParams();
+    const { isAuth, setIsAuth } = useAuth();
 
-}
+    const target = searchParams.get("prevUrl");
+    const prevUrl = target ? target : "/";
 
-const LoginForm = () => {
-  const navigate = useNavigate();
-  const { isAuth, setIsAuth } = useAuth();
+    const actionWithSetter = loginUserAction.bind(null, isAuth, setIsAuth, navigate, prevUrl)
+    const [state, formAction, isPending] = useActionState(actionWithSetter, null);
+    
 
-  const actionWithSetter = loginUserAction.bind(null, isAuth, setIsAuth, navigate)
-  const [state, formAction, isPending] = useActionState(actionWithSetter, null);
-  
+    return(
+      <form action={formAction}>
+          <label htmlFor="user_name">Email:</label>
+          <input type="email" name="user_name" id="user_name" required />
 
-  return(
-    <form action={formAction}>
-        <label htmlFor="user_name">Email:</label>
-        <input type="email" name="user_name" id="user_name" required />
+          <label htmlFor="password">Password:</label>
+          <input type="password" name="password" id="password" required />
 
-        <label htmlFor="password">Password:</label>
-        <input type="password" name="password" id="password" required />
+          <button type="submit" disabled={isPending}>
+              {isPending? "Logging in..." : "Login"}
+          </button>
 
-        <button type="submit" disabled={isPending}>
-            {isPending? "Logging in..." : "Login"}
-        </button>
+          {state?.error && <p style={{ color: 'red'}}>{state.error}</p>}
+          {state?.success && <p>Login successful! {state.message}</p>}
+      </form>
+    )
+  }   
 
-        {state?.error && <p style={{ color: 'red'}}>{state.error}</p>}
-        {state?.success && <p>Login successful! {state.message}</p>}
-    </form>
-  )
-}   
+  export default LoginForm;
 
-export default LoginForm;
-
-// Basic example using only `action` attribute and action function
-// import authUser from "../api/authUser";
+  // Basic example using only `action` attribute and action function
+  // import authUser from "../api/authUser";
 
 
-// async function loginUser(formData) {
-//   console.log(formData);
-//   const data = {
-//     user_name: formData.get("user_name"),
-//     password: formData.get("password"),
-//   }
-//   console.log(data);
-  
-//   try {
-//     await authUser(data);
-//   } catch (error) {
-//    console.log(error); 
-//   }
-// }
+  // async function loginUser(formData) {
+  //   console.log(formData);
+  //   const data = {
+  //     user_name: formData.get("user_name"),
+  //     password: formData.get("password"),
+  //   }
+  //   console.log(data);
+    
+  //   try {
+  //     await authUser(data);
+  //   } catch (error) {
+  //    console.log(error); 
+  //   }
+  // }
 
-// const LoginForm = () => {
-//   return(
-//     <form action={loginUser}>
-//       <label htmlFor="user_name">Email:</label>
-//       <input type="email" name="user_name" id="user_name" required />
+  // const LoginForm = () => {
+  //   return(
+  //     <form action={loginUser}>
+  //       <label htmlFor="user_name">Email:</label>
+  //       <input type="email" name="user_name" id="user_name" required />
 
-//       <label htmlFor="password">Password:</label>
-//       <input type="password" name="password" id="password" required />
+  //       <label htmlFor="password">Password:</label>
+  //       <input type="password" name="password" id="password" required />
 
-//       <button type="submit">Login</button>
-//     </form>
-//   )
-// }
+  //       <button type="submit">Login</button>
+  //     </form>
+  //   )
+  // }
 
-// export default LoginForm;
+  // export default LoginForm;
