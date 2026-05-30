905ba28b1cdd016884a95f0d0f57f2c8179ba6d9
Josef Tinagan <tinaganjosefperry@gmail.com>
2026-05-30 17:26:23 +0800

    fix: bug on the onblur handler by copying the error state object locally and manually adding the new error on it, uses the local object for logic checks to make the submit button toggle between enabled and disabled

--- src/pages/sign-in/ui/RegisterForm.jsx
+++ src/pages/sign-in/ui/RegisterForm.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useState } from "react";
 
 const RegisterForm = () => {
   const [formData, setFormData] = useState({
@@ -16,11 +16,8 @@ const RegisterForm = () => {
 
   const [loading, setLoading] = useState(false);
 
-  const [hasErrors, setHasErrors] = useState(false);
+  const [hasErrors, setHasErrors] = useState(true);
 
-  const errorsRef = useRef(errors);
-
-  useEffect(() => { errorsRef.current = errors}, [errors]);
 
   const validateField = (fieldName, fieldValue) => {
     console.log("Validating...");
@@ -31,33 +28,11 @@ const RegisterForm = () => {
 
     if (fieldName === 'email'){
       const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      if (!fieldValue) currentError ='Email is required';
+      if (!fieldValue) currentError = 'Email is required';
       else if (!emailRegex.test(fieldValue)) currentError = 'Invalid email format';
     }
 
     return currentError;
-
-    // setErrors(prevErrors => ({
-    //    ...prevErrors,
-    //    [fieldName]: null
-    // }));
-
-    // if(fieldName === "email"){
-    //   if(!(fieldValue.includes("@"))){
-    //     setErrors(prevErrors => ({
-    //       ...prevErrors,
-    //       [fieldName]: "Invalid email format, email must include @"
-    //     }));
-    //   }
-
-    //   if(fieldValue === ""){
-    //     setErrors(prevErrors => ({
-    //       ...prevErrors,
-    //       [fieldName]: "Email field must not be empty"
-    //     }));
-    //   }
-    // }
-
     
   }
 
@@ -78,23 +53,20 @@ const RegisterForm = () => {
       [name]: currentError
     }));
 
-    console.log("Current errors object list");
-    console.log(errors);
+    let errorObj = {...errors}
+    errorObj = {...errors, [name]: currentError};
 
-    // if(currentError){
-    //   console.log("Has error");
-    //   console.log(currentError);
-    //   setHasErrors(true);
-    // } else {
-    //   console.log("No error");
-    //   setHasErrors(false);
-    // }
-
-    console.log("ref current value:");
-    console.log(errorsRef.current);
-    for( const [key, value] of Object.entries(errorsRef.current)){
+    setHasErrors(false);
+    for( const [key, value] of Object.entries(errorObj)){
       console.log(`${key}: ${value}`);
       if(value){
+        setHasErrors(true);
+      }
+    }
+
+    for(const [key, value] of Object.entries(formData)){
+      console.log(`${key}: ${value}`);
+      if(value === ''){
         setHasErrors(true);
       }
     }
