f94975e4f0242b8e4d654b8ca9deb283ec25aa97
Josef Tinagan <tinaganjosefperry@gmail.com>
2026-05-31 08:26:39 +0800

    feat: refactor register form to just use the error state for toggling the enabled/disabled state of the submit button, remove unnecessary state

--- src/pages/sign-in/ui/RegisterForm.jsx
+++ src/pages/sign-in/ui/RegisterForm.jsx
@@ -4,25 +4,32 @@ const RegisterForm = () => {
   const [formData, setFormData] = useState({
     email: "",
     password: "",
-    confirm_password: "",
-    firstName: "",
-    lastName: "",
+
   });
 
-  const [errors, setErrors] = useState({
-    email: null,
-    password: null,
-  })
-
+  const [errors, setErrors] = useState({});
   const [loading, setLoading] = useState(false);
 
-  const [hasErrors, setHasErrors] = useState(true);
+  // Use to disable/enable the submit button, disables the submit button if it returns true, enables the return button if it retuns false
+  const checkSubmitButton = () => {
+    if(Object.keys(errors).length !== 0){
+      return true;
+    }
 
+    for(const [key, value] of Object.entries(formData)){
+      if(value === ''){
+        return true;
+      }
+    }
 
+    return false;
+  }
+
+  // Helper function that validates inputs, return the error if there is one
   const validateField = (fieldName, fieldValue) => {
     console.log("Validating...");
-    console.log(fieldName);
-    console.log(fieldValue);
+    // console.log(fieldName);
+    // console.log(fieldValue);
 
     let currentError = null;
 
@@ -48,34 +55,27 @@ const RegisterForm = () => {
     const { name, value } = e.target;
 
     const currentError = validateField(name, value);
-    setErrors(prevErrors => ({
-      ...prevErrors,
-      [name]: currentError
-    }));
+    console.log("Current error:");
+    console.log(currentError);
 
-    let errorObj = {...errors}
-    errorObj = {...errors, [name]: currentError};
+    if(currentError){
+      setErrors(prevErrors => ({
+        ...prevErrors,
+        [name]: currentError
+      }));
+    } else {
+      setErrors(prevErrors => {
+        const { [name]: removedField, ...rest } = prevErrors;
+        return rest;
+      })
 
-    setHasErrors(false);
-    for( const [key, value] of Object.entries(errorObj)){
-      console.log(`${key}: ${value}`);
-      if(value){
-        setHasErrors(true);
-      }
-    }
-
-    for(const [key, value] of Object.entries(formData)){
-      console.log(`${key}: ${value}`);
-      if(value === ''){
-        setHasErrors(true);
-      }
     }
   }
 
   const onSubmitHandler = (e) => {
     e.preventDefault();
     console.log("Submit handler triggered...");
-    console.log(e.target);
+    // console.log(e.target);
     // for( const [key, value] of Object.entries(errors)){
     //   console.log(`${key}: ${value}`);
     // }
@@ -85,14 +85,17 @@ const RegisterForm = () => {
     <form onSubmit={onSubmitHandler}>
         <label htmlFor="email">Email:</label>
         <input type="text" name="email" id="email" value={formData.email} onChange={onChangeHandler} onBlur={onBlurHandler}/>
-        { errors.email && <span>{errors.email}</span>}
+        { errors && errors.email && <span>{errors.email}</span>}
 
         <label htmlFor="password">Password:</label>
         <input type="password" name="password" id="password" value={formData.password} onChange={onChangeHandler} onBlur={onBlurHandler} />
 
-        <button type="submit" disabled={loading || hasErrors}>{ hasErrors ? 'Register' : loading ? 'Registration in process...' : 'Register'}</button>
+        
+
+        <button type="submit" disabled={loading || checkSubmitButton()}>{ checkSubmitButton() ? 'Register' : loading ? 'Registration in process...' : 'Register'}</button>
     </form>
   )
 }
+
 
 export default RegisterForm;
