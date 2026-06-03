d5d58e5fed86003edb42eeb22b1949f09151516f
Josef Tinagan <tinaganjosefperry@gmail.com>
2026-06-03 11:35:35 +0800

    refactor: instead of using state to check if a new comment is added, use tanstack query's invalidateQueries method to trigger the refetch instead

--- src/pages/blogs-read/ui/CommentForm.jsx
+++ src/pages/blogs-read/ui/CommentForm.jsx
@@ -1,23 +1,26 @@
 import { useActionState, useState } from "react";
+import { useQueryClient } from "@tanstack/react-query";
 
 import postCreateComment from "../api/postCreateComment";
 
 
-async function postCommentAction(publicPostId, setIsNewComment, prevState, formData){
+async function postCommentAction(publicPostId, setIsNewComment, queryClient, prevState, formData){
   console.log("post comment action...");
   console.log(publicPostId);
   const text = formData.get("text");
   await postCreateComment(publicPostId, text);
-  setIsNewComment(true);
+  // Either use the state setter to check if there's a new comment or if using Tanstack Query, use `queryClient.invalidateQueries` and pass on the query key you want to invalidate and refetch
+  // setIsNewComment(true);
+  queryClient.invalidateQueries({ queryKey: ['specificPostComments']});
 }
 
 const CommentForm = ( {publicPostId, setIsNewComment}) => {
-
+  const queryClient = useQueryClient();
 
   console.log("Comment form....");
 
 
-  const bindedAction = postCommentAction.bind(null, publicPostId, setIsNewComment)
+  const bindedAction = postCommentAction.bind(null, publicPostId, setIsNewComment, queryClient)
   const [state, formAction, isPending] = useActionState(bindedAction, null);
   
 
