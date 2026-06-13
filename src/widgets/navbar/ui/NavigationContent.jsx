1e40dc5e84b7e4115d11aa50303b096b31362593
Josef Tinagan <tinaganjosefperry@gmail.com>
2026-06-13 09:48:09 +0800

    fix: add text header logo for the header in the navigation bar

--- src/widgets/navbar/ui/NavigationContent.jsx
+++ src/widgets/navbar/ui/NavigationContent.jsx
@@ -2,11 +2,13 @@ import { NavLink } from "react-router";
 
 import NavigationLinks from "./NavigationLinks";
 import styles from "./NavigationContent.module.css";
+import logo from "./../../../shared/assets/Josef.png"
 
 const NavigationContent = () => {
   return(
     <div className={styles.flexContainer}>
-      <div><NavLink to="/">Logo</NavLink></div>
+      {/* <div className={styles.headerLogo}><NavLink to="/">Josef</NavLink></div> */}
+      <div><NavLink to="/"><img src={logo} alt="Header Logo" srcset="" /></NavLink></div>
       <nav>
         <NavigationLinks />
       </nav>
