0d7f915f75ad7c86ecbcb5f95541fe62389ae817
Josef Tinagan <tinaganjosefperry@gmail.com>
2026-06-13 19:42:00 +0800

    feat: add a hero section using gradient background and some text

--- src/pages/home/ui/HomePage.jsx
+++ src/pages/home/ui/HomePage.jsx
@@ -4,12 +4,16 @@ import styles from "./HomePage.module.css";
 export default function HomePage(){
   return(
     <div className={styles.landingPageContainer}>
-      <div className={styles.heroImgContainer}>
-        Hero Image
+      <div className={styles.heroSectionContainer}>
+        <p>Aspiring Developer</p>
+        <h2>Hi, I'm Josef! I am an aspiring developer and create websites for a hobby</h2>
       </div>
-      <div className={styles.heroTextContainer}>
-        Hero Text
-      </div>
+      {/* <div className={styles.heroImgContainer}>
+          Hero Image
+        </div>
+        <div className={styles.heroTextContainer}>
+          Hero Text
+      </div> */}
       <div className={styles.featuredPostContainer}>
         <h3>Featured Posts</h3>
         <FeaturedPostsList />
