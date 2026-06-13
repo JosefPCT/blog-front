7d21ba74c3213221393844db0f93284ed2a291ac
Josef Tinagan <tinaganjosefperry@gmail.com>
2026-06-13 13:20:25 +0800

    feat: structure the homepage with grid

--- src/pages/home/ui/HomePage.jsx
+++ src/pages/home/ui/HomePage.jsx
@@ -1,10 +1,27 @@
 import FeaturedPostsList from "./FeaturedPostsList"
+import styles from "./HomePage.module.css";
 
 export default function HomePage(){
   return(
-    <>
-      <h1>Latest Posts</h1>
-      <FeaturedPostsList />
-    </>
+    <div className={styles.landingPageContainer}>
+      <div className={styles.heroImgContainer}>
+        Hero Image
+      </div>
+      <div className={styles.heroTextContainer}>
+        Hero Text
+      </div>
+      <div className={styles.featuredPostContainer}>
+        <h3>Featured Posts</h3>
+        <FeaturedPostsList />
+      </div>
+      <div className={styles.latestPostsContainer}>
+        <h3>Latest Posts</h3>
+      </div>
+      <div className={styles.newsletterContainer}>
+        Newsletter
+      </div>
+  
+      
+    </div>
   )
 }
