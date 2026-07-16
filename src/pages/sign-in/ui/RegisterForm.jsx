7db1ce42ae6cd76a9b500d127ffcdb9b2e1869e4
Josef Tinagan <tinaganjosefperry@gmail.com>
2026-07-16 17:15:22 +0800

    feat: refactor show password buttons to use svg to toggle

--- src/pages/sign-in/ui/RegisterForm.jsx
+++ src/pages/sign-in/ui/RegisterForm.jsx
@@ -136,18 +136,53 @@ const RegisterForm = ({setToggleSection}) => {
         <input type="text" name="email" id="email" value={formData.email} onChange={onChangeHandler} onBlur={onBlurHandler}/>
         { errors && errors.email && <span>{errors.email}</span>}
 
-        <label htmlFor="password">Password</label>
+        <div className={`${styles.showPasswordContainer}`}>
+          <label htmlFor="password">Password</label>
+          { showPassword ? 
+            <button onClick={showPasswordToggle}>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                <circle cx="12" cy="12" r="3"></circle>
+              </svg>
+            </button> :
+            <button onClick={showPasswordToggle}>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24"></path>
+                <line x1="1" y1="1" x2="23" y2="23"></line>
+              </svg>
+            </button>
+          }
+        </div>
+        
         { showPassword ? 
           <input type="text" name="password" id="password" value={formData.password} onChange={onChangeHandler} onBlur={onBlurHandler} />
           : <input type="password" name="password" id="password" value={formData.password} onChange={onChangeHandler} onBlur={onBlurHandler} /> }
-        <button onClick={showPasswordToggle}>{ showPassword ? "Hide Password" : "Show Password"}</button>
+          
+        {/* <button onClick={showPasswordToggle}>{ showPassword ? "Hide Password" : "Show Password"}</button> */}
         { errors && errors.password && <span>{errors.password}</span>}
 
-        <label htmlFor="confirm_password">Confirm Password</label>
+        <div className={`${styles.showPasswordContainer}`}>
+          <label htmlFor="confirm_password">Confirm Password</label>
+          { showConfirmPassword ? 
+            <button onClick={showConfirmPasswordToggle}>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                <circle cx="12" cy="12" r="3"></circle>
+              </svg>
+            </button> :
+            <button onClick={showConfirmPasswordToggle}>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24"></path>
+                <line x1="1" y1="1" x2="23" y2="23"></line>
+              </svg>
+            </button>
+          }
+        </div>
         { showConfirmPassword ?
           <input type="text" name="confirm_password" id="confirm_password" value={formData.confirm_password} onChange={onChangeHandler} onBlur={onBlurHandler} />
           : <input type="password" name="confirm_password" id="confirm_password" value={formData.confirm_password} onChange={onChangeHandler} onBlur={onBlurHandler} /> }
-        <button onClick={showConfirmPasswordToggle}>{ showConfirmPassword ? "Hide Password" : "Show Password"}</button>
+
+        {/* <button onClick={showConfirmPasswordToggle}>{ showConfirmPassword ? "Hide Password" : "Show Password"}</button> */}
         { errors && errors.confirm_password && <span>{errors.confirm_password}</span>}
 
         <label htmlFor="firstName">First Name</label>
