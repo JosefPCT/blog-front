5476c575e3a2112f947d4ebb10da07fbbf8c6e61
Josef Tinagan <tinaganjosefperry@gmail.com>
2026-07-16 08:44:40 +0800

    feat: add basic structuring for sign-in page including each section

--- src/pages/sign-in/ui/LoginSection.jsx
+++ src/pages/sign-in/ui/LoginSection.jsx
@@ -7,6 +7,8 @@ import LoginForm from "./LoginFormReact19";
 
 import RegisterForm from "./RegisterForm";
 
+import styles from "./LoginSection.module.css";
+
 const LoginSection = () => {
   const [toggleSection, setToggleSection] = useState("login");
 
@@ -14,10 +16,25 @@ const LoginSection = () => {
     setToggleSection(prevValue => prevValue === 'login' ? 'register' : 'login');
   }
   return(
-    <>
-      <button onClick={toggleSectionHandler}>{ toggleSection === 'login' ? 'Go to registration' : 'Go to login'}</button>
-      { toggleSection === 'login' ? <LoginForm /> : <RegisterForm setToggleSection={setToggleSection}/>}
-    </>
+    <div className={styles.sectionContainer}>
+      <div className={styles.mainContentContainer}>
+        <div>
+          <h2>Brand</h2>
+        </div>
+        <div className={styles.formSection}>
+          <h3>Welcome</h3>
+          <p>Enter your details</p>
+          <button onClick={toggleSectionHandler}>{ toggleSection === 'login' ? 'Go to registration' : 'Go to login'}</button>
+          { toggleSection === 'login' ? <LoginForm /> : <RegisterForm setToggleSection={setToggleSection}/>}
+        </div>
+        <div>
+          Footer
+        </div>
+      </div>
+      <div className={styles.mainImageContainer}>
+        <img src="https://images.unsplash.com/photo-1783961797133-dd0fc5a722ec?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Image  " />
+      </div>
+    </div>
   )
 }
 
--- src/pages/sign-in/ui/SignInPage.jsx
+++ src/pages/sign-in/ui/SignInPage.jsx
@@ -1,11 +1,13 @@
 // import Login from "./Login";
 import LoginSection from "./LoginSection";
 
+import styles from "./SignInPage.module.css";
+
 const SignInPage = () => {
   return(
-    <>
+    <div className={styles.pageContainer}>
       <LoginSection />
-    </>
+    </div>
   )
 }
 
